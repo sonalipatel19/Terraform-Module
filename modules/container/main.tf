1449f01bbd378ab2e869002bb60af2e64ef8538a
sonalipatel19 <sonali@gmail.com>
2025-12-02 18:09:20 -0500

    add var.tf file

--- modules/container/main.tf
+++ modules/container/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
-  storage_account_id    = azurerm_storage_account.storageaccount.id
+  storage_account_id    = var.storage_account_id
   container_access_type = var.access_type
 }
