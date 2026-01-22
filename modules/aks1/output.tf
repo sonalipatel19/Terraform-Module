0682f70e03cdaf210f344a8988d6b4f41b004bf4
sonalipatel19 <you@example.com>
2026-01-21 22:08:58 -0500

    add file

--- modules/aks1/output.tf
+++ modules/aks1/output.tf
@@ -2,6 +2,7 @@ output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.raw_kube_config
+output "aks_kube_config_raw" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
